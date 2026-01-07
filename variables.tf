920c98110fc21a259df5df599de404cbc69b054f
Rohith Racharla <rohith.racharla@gmail.com>
2026-01-07 21:55:54 +0530

    Create variables.tf file for storing variables

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "myIP" {
+  type = string
+  sensitive = true
+}
+
+variable "public_key" {
+  type = string
+  sensitive = true
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "web_count" {
+  type = number
+  default = 3
+}
