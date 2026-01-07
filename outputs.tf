4348a1eec4eb7ecbad547a6579093fc5e5c4ab52
Rohith Racharla <rohith.racharla@gmail.com>
2026-01-07 20:26:26 +0530

    Update: included vpc id output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instance_id" {
   description = "AMI ID of Ubuntu instance"
   value       = data.aws_ami.amiID.id
 }
+
+output "vpc_id" {
+  description = "VPC ID of our custom vpc"
+  value       = module.vpc.vpc_id
+}
