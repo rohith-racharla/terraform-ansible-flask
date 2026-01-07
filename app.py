32658e3accbdaf73d6a4f75767260a921e66557b
Rohith Racharla <rohith.racharla@gmail.com>
2026-01-07 20:21:28 +0530

    Create Flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+import flask
+import time
+import socket
+
+h_name = socket.gethostname()
+IP_address = socket.gethostbyname(h_name)
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    Time= time.strftime("%H:%M:%S")
+    return Time+" Serving from "+h_name+" ("+IP_address+")\n"
