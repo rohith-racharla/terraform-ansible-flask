0274a30bb95d25e6ec96e7dd47368f7faa69c4d3
Rohith Racharla <rohith.racharla@gmail.com>
2026-01-14 01:48:51 +0530

    Update vars.sh to include web count variable from user input

--- vars.sh
+++ vars.sh
@@ -10,14 +10,16 @@ echo "$(date) Gathering required Terraform variables to generate terraform.tfvar
 read -rp "AWS Region (e.g. us-east-1): " REGION
 read -rp "AWS Availability Zone (e.g. us-east-1a): " AZ
 read -rp "Instance type (t2.micro / t3.micro): " INSTANCE_TYPE
+read -rp "Web servers count (e.g. 3): " COUNT
 
 MY_IP=$(curl -s https://api.ipify.org)
 
 cat <<EOF > terraform.tfvars
 region        = "${REGION}"
-instance_type = "${INSTANCE_TYPE}"
 myIP          = "${MY_IP}/32"
 public_key    = "$(cat ~/.ssh/id_rsa.pub)"
+instance_type = "${INSTANCE_TYPE}"
+web_count     = "${COUNT}"
 az            = "${AZ}"
 EOF
 
