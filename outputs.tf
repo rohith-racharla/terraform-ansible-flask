391140e9815201fb859d1d1fc499947504a1e7a8
Rohith Racharla <rohith.racharla@gmail.com>
2026-01-14 00:46:15 +0530

    Update outputs.tf to output IPs of hosts

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,16 @@ output "vpc_id" {
   description = "VPC ID of our custom vpc"
   value       = module.vpc.vpc_id
 }
+
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
+output "haproxy_private_ip" {
+  value = aws_instance.haproxy.private_ip
+}
+
+output "web_private_ips" {
+  description = "Private IPs of web servers"
+  value       = aws_instance.web[*].private_ip
+}
